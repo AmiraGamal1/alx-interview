5d2b3d886a251c0ecb6e0649ea23e8f5aef7e045
AmiraGamal1 <amiragamal098@gmail.com>
2024-10-02 12:11:01 +0300

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
 def pascal_triangle(n):
+    """Generate Pascal's Triangle up to the nth row."""
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    elif n == 2:
+        return [[1], [1,1]]
+    else:
+        pascal = [[1], [1,1]]
+        for i in range(2, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(pascal[i-1][j-1] + pascal[i-1][j])
+            row.append(1)
+            pascal.append(row)
+        return pascal
