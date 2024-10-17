6d20628c200eaa4f6c3dc5000c277e43d1f819de
AmiraGamal1 <amiragamal098@gmail.com>
2024-10-18 00:35:42 +0300

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+'''Coding challenge.
+'''
+
+
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    if n == 1:
+        return 0
+    if n == 2:
+        return 2
+    if n == 4:
+        return 4
+    if n % 2 == 0:
+        if n % 4 == 0:
+            return (n // 4 + 4 )
+        else:
+            return (n // 2 + 2)
+    else:
+        return 0
