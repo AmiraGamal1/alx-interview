5edc3a7ebd7444bf0439c1df3df08f9a6f4d1760
AmiraGamal1 <amiragamal098@gmail.com>
2024-10-23 12:41:40 +0300

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,76 +1,59 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics"""
+"""Reads from standard input and computes metrics.
 
-import re
-import sys
-
-
-def extract_input(line):
-    """Extracts input from standard input.
-    Args:
-        line (str): Input from standard input
-    """
-    regex = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+-\d+-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-
-    info = {
-        'status_code': '0',
-        'file_size': 0,
-    }
-
-    fmt = '{}\\-{}{}{}{}\\s*'.format(regex[0], regex[1],
-                                     regex[2], regex[3], regex[4])
-    match = re.fullmatch(fmt, line)
-    if match is not None:
-        status_code = match.group('status_code')
-        file_size = int(match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-
-    return info
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 
-def print_metrics(size, status_codes):
-    """Print metrics.
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
     Args:
-        size (int): the accumulated read file size.
-        status_codes (dict): the accumulated count of status codes.
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
-    for key in sorted(status_codes.keys()):
+    for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
+    import sys
+
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
+
     try:
         for line in sys.stdin:
             if count == 10:
-                print_metrics(size, status_codes)
+                print_stats(size, status_codes)
                 count = 1
             else:
                 count += 1
-            info = extract_input(line)
-            file_size = info.get('file_size', '0')
-            status_code = info.get('status_code', '0')
+
+            line = line.split()
+
             try:
-                if status_code in valid_codes:
-                    if status_codes.get(status_code, -1) == -1:
-                        status_codes[status_code] = 1
-                    else:
-                        status_codes[status_code] += 1
-                    size += int(file_size)
+                size += int(line[-1])
             except (IndexError, ValueError):
                 pass
-    except (KeyboardInterrupt, EOFError):
-        print_metrics(size, status_codes)
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
         raise
