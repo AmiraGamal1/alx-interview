e330cd588bc95d7ddda4bb74ffe8509e7cd81e35
AmiraGamal1 <amiragamal098@gmail.com>
2024-10-23 12:04:48 +0300

    req

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
 """Reads from standard input and computes metrics"""
+
 import re
+import sys
 
 
 def extract_input(line):
-    """Extracts input from standard input
-       Args:
-            line (str): Input from standard input
+    """Extracts input from standard input.
+    
+    Args:
+        line (str): Input from standard input
     """
     regex = (
         r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*\[(?P<date>\d+-\d+-\d+ \d+:\d+:\d+\.\d+)\]',
         r'\s*"(?P<request>[^"]*)"\s*',
         r'\s*(?P<status_code>\S+)',
         r'\s*(?P<file_size>\d+)'
     )
+
     info = {
         'status_code': 0,
         'file_size': 0,
     }
-    fmt = '{}\\-{}{}{}{}\\s*'.format(regex[0], regex[1], regex[2], regex[3], regex[4])
+
+    fmt = '{}-{}{}{}{}\s*'.format(regex[0], regex[1], regex[2], regex[3], regex[4])
+    
     match = re.fullmatch(fmt, line)
     if match is not None:
         status_code = match.group('status_code')
         file_size = int(match.group('file_size'))
         info['status_code'] = status_code
         info['file_size'] = file_size
+    
     return info
 
 
 def print_metrics(size, status_codes):
-    """print metrics
+    """Print metrics.
 
     Args:
         size (int): the accumulated read file size.
@@ -42,13 +49,10 @@ def print_metrics(size, status_codes):
 
 
 if __name__ == "__main__":
-    import sys
-
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
-
     try:
         for line in sys.stdin:
             if count == 10:
@@ -56,24 +60,17 @@ if __name__ == "__main__":
                 count = 1
             else:
                 count += 1
-
             info = extract_input(line)
             file_size = info.get('file_size', '0')
             status_code = info.get('status_code', '0')
-
-            try:
-                size += int(file_size)
-            except (IndexError, ValueError):
-                pass
-
             try:
                 if status_code in valid_codes:
                     if status_codes.get(status_code, -1) == -1:
                         status_codes[status_code] = 1
                     else:
                         status_codes[status_code] += 1
-            except IndexError:
+                    size += int(file_size)
+            except (IndexError, ValueError):
                 pass
-
     except (KeyboardInterrupt, EOFError):
         print_metrics(size, status_codes)
